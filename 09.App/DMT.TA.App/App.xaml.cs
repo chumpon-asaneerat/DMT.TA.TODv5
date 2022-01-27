c1e581eeed3ee87cf669a10fa306230b8634c967
chumpon-asaneerat <chumponsenate@gmail.com>
2022-01-27 09:10:42 +0700

    Update 2022-01-27 #8 - DMT.TAxTODv5   - DMT.TA.App project     - Update App.xaml.cs       - Implements OnStartup method.       - Implements OnExit method.

--- 09.App/DMT.TA.App/App.xaml.cs
+++ 09.App/DMT.TA.App/App.xaml.cs
@@ -4,15 +4,15 @@
 
 using System;
 using System.Windows;
-/*
+
 using NLib;
 using NLib.Logs;
 
-using DMT.Configurations;
-using DMT.Models;
-using DMT.Models.ExtensionMethods;
-using DMT.Services;
-*/
+//using DMT.Configurations;
+//using DMT.Models;
+//using DMT.Models.ExtensionMethods;
+//using DMT.Services;
+
 #endregion
 
 namespace DMT
@@ -32,7 +32,7 @@ namespace DMT
         {
             base.OnStartup(e);
 
-            //Console.WriteLine("OnStartUp");
+            #region Check CurrentContext
 
             if (null != AppDomain.CurrentDomain)
             {
@@ -40,6 +40,65 @@ namespace DMT
                 {
                     Console.WriteLine("Thread CurrentContext is not null.");
                 }
+            }
+
+            #endregion
+
+            #region Create Application Environment Options
+
+            EnvironmentOptions option = new EnvironmentOptions()
+            {
+                /* Setup Application Information */
+                AppInfo = new NAppInformation()
+                {
+                    /*  This property is required */
+                    CompanyName = "DMT",
+                    /*  This property is required */
+                    ProductName = AppConsts.Application.TA.ApplicationName,
+                    /* For Application Version */
+                    Version = AppConsts.Application.TA.Version,
+                    Minor = AppConsts.Application.TA.Minor,
+                    Build = AppConsts.Application.TA.Build,
+                    LastUpdate = AppConsts.Application.TA.LastUpdate
+                },
+                /* Setup Storage */
+                Storage = new NAppStorage()
+                {
+                    StorageType = NAppFolder.Application
+                },
+                /* Setup Behaviors */
+                Behaviors = new NAppBehaviors()
+                {
+                    /* Set to true for allow only one instance of application can execute an runtime */
+                    IsSingleAppInstance = true,
+                    /* Set to true for enable Debuggers this value should always be true */
+                    EnableDebuggers = true
+                }
+            };
+
+            #endregion
+
+            #region Setup Option to Controller and check instance
+
+            WpfAppContoller.Instance.Setup(option);
+
+            if (option.Behaviors.IsSingleAppInstance &&
+                WpfAppContoller.Instance.HasMoreInstance)
+            {
+                return;
+            }
+
+            #endregion
+
+            // Start log manager
+            LogManager.Instance.Start();
+
+            Window window = null;
+            window = new MainWindow();
+
+            if (null != window)
+            {
+                WpfAppContoller.Instance.Run(window);
             }
         }
 
@@ -53,7 +112,14 @@ namespace DMT
         /// <param name="e"></param>
         protected override void OnExit(ExitEventArgs e)
         {
-            //Console.WriteLine("OnExit");
+            // Shutdown log manager
+            LogManager.Instance.Shutdown();
+
+            // Wpf shutdown process required exit code.
+
+            /* If auto close the single instance must be true */
+            bool autoCloseProcess = true;
+            WpfAppContoller.Instance.Shutdown(autoCloseProcess, e.ApplicationExitCode);
 
             base.OnExit(e);
         }
