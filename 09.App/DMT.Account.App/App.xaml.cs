0912803b89941197ac6beb247d13009e5fe6bca9
chumpon-asaneerat <chumponsenate@gmail.com>
2022-01-27 08:18:58 +0700

    Update 2022-01-27 #1 - DMT.TAxTODv5   - DMT.Account.App project created and assigned App.config, Icon, app.manifest and embeded smartcard dlls.   - DMT.TA.App project created and assigned App.config, Icon, app.manifest and embeded smartcard dlls.   - DMT.TOD.App project created and assigned App.config, Icon, app.manifest and embeded smartcard dlls.   - DMT.Plaza.Config.App project created and assigned App.config, Icon, app.manifest   - DMT.Plaza.Simulator.App project created and assigned App.config, Icon, app.manifest

---
+++ 09.App/DMT.Account.App/App.xaml.cs
@@ -0,0 +1,63 @@
+﻿#define SINGELTON_APP
+
+#region Using
+
+using System;
+using System.Windows;
+/*
+using NLib;
+using NLib.Logs;
+
+using DMT.Configurations;
+using DMT.Models;
+using DMT.Models.ExtensionMethods;
+using DMT.Services;
+*/
+#endregion
+
+namespace DMT
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        #region OnStartup
+
+        /// <summary>
+        /// OnStartup.
+        /// </summary>
+        /// <param name="e"></param>
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            //Console.WriteLine("OnStartUp");
+
+            if (null != AppDomain.CurrentDomain)
+            {
+                if (null != System.Threading.Thread.CurrentContext)
+                {
+                    Console.WriteLine("Thread CurrentContext is not null.");
+                }
+            }
+        }
+
+        #endregion
+
+        #region OnExit
+
+        /// <summary>
+        /// OnExit.
+        /// </summary>
+        /// <param name="e"></param>
+        protected override void OnExit(ExitEventArgs e)
+        {
+            //Console.WriteLine("OnExit");
+
+            base.OnExit(e);
+        }
+
+        #endregion
+    }
+}
