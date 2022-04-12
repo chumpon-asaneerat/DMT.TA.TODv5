53997d5adab874acb7cb234f07ccf81b3907e124
chumpon-asaneerat <chumponsenate@gmail.com>
2022-04-12 09:24:41 +0700

    Update 2022-04-12 #9 - DMT.TAxTODv5   - DMT.Models project     - Prepare convert classes for old models in v1 #1

---
+++ 02.Models/DMT.Models/__ModelsV1/Models.V1.cs
@@ -0,0 +1,25 @@
+﻿#region Using
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+
+using NLib;
+using NLib.Design;
+using NLib.Reflection;
+
+using SQLite;
+using SQLiteNetExtensions.Attributes;
+using SQLiteNetExtensions.Extensions;
+// required for JsonIgnore attribute.
+using Newtonsoft.Json;
+using Newtonsoft.Json.Bson;
+using System.Reflection;
+
+#endregion
+
+namespace DMT.Models.V1
+{
+
+}
