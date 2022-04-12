3b30f04f16dd5f3cd235a6179be7677e45cf22ae
chumpon-asaneerat <chumponsenate@gmail.com>
2022-04-12 08:42:55 +0700

    Update 2022-04-12 #1 - DMT.TAxTODv5   - DMT.Core project     - implements NInpc class #1

---
+++ 02.Models/DMT.Models/Models/Common/DMTModelBase.cs
@@ -0,0 +1,21 @@
+﻿#region Using
+
+using System.ComponentModel;
+using NLib;
+
+// required for JsonIgnore attribute.
+using Newtonsoft.Json;
+using Newtonsoft.Json.Bson;
+
+#endregion
+
+namespace DMT.Models
+{
+    #region DMTModelBase (abstract)
+
+    class DMTModelBase
+    {
+    }
+
+    #endregion
+}
