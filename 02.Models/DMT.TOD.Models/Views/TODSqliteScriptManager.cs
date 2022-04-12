e1dcb79cb7c0f3abae114c5e72a77e5522b00264
chumpon-asaneerat <chumponsenate@gmail.com>
2022-04-12 09:19:38 +0700

    Update 2022-04-12 #8 - DMT.TAxTODv5   - DMT.Account.Models project     - Add AccountSqliteScriptManager class.   - DMT.Plaza.Models project     - Add PlazaSqlScriptManager class.   - DMT.TA.Models project     - Add TASqliteScriptManager class.   - DMT.TOD.Models project     - Add TODSqliteScriptManager class.

---
+++ 02.Models/DMT.TOD.Models/Views/TODSqliteScriptManager.cs
@@ -0,0 +1,58 @@
+﻿#region Using
+
+using System;
+using System.IO;
+using System.Reflection;
+using NLib;
+
+#endregion
+
+namespace DMT.Views
+{
+    #region TODSqliteScriptManager
+
+    /// <summary>
+    /// The TOD Sqlite Script Manager class.
+    /// </summary>
+    public class TODSqliteScriptManager
+    {
+        private static Assembly Current { get { return typeof(TODSqliteScriptManager).Assembly; } }
+        /// <summary>
+        /// Gets View SQL Script (from embedded resource).
+        /// </summary>
+        /// <param name="resourceName">The resource name.</param>
+        /// <returns>Returns load view sql script.</returns>
+        public static string GetScript(string resourceName)
+        {
+            MethodBase med = MethodBase.GetCurrentMethod();
+            string ret = string.Empty;
+            if (!string.IsNullOrWhiteSpace(resourceName))
+            {
+                try
+                {
+                    using (Stream stream = Current.GetManifestResourceStream(resourceName))
+                    {
+                        if (null != stream)
+                        {
+                            using (StreamReader reader = new StreamReader(stream))
+                            {
+                                if (null != reader)
+                                {
+                                    ret = reader.ReadToEnd();
+                                }
+                            }
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    med.Err(ex);
+                    ret = string.Empty;
+                }
+            }
+            return ret;
+        }
+    }
+
+    #endregion
+}
