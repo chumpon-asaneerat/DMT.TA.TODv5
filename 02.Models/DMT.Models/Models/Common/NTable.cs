2d55e2f90bca6cb181948924251b16c89fb8b503
chumpon-asaneerat <chumponsenate@gmail.com>
2022-04-12 09:13:15 +0700

    Update 2022-04-12 #6 - DMT.TAxTODv5   - DMT.Models project     - Add NTable class.

---
+++ 02.Models/DMT.Models/Models/Common/NTable.cs
@@ -0,0 +1,45 @@
+﻿#region Using
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Media;
+
+using SQLite;
+using SQLiteNetExtensions.Attributes;
+using SQLiteNetExtensions.Extensions;
+using System.ComponentModel;
+//using DMT.Services;
+// required for JsonIgnore.
+using Newtonsoft.Json;
+using NLib;
+using NLib.Reflection;
+using System.Reflection;
+
+#endregion
+
+namespace DMT.Models
+{
+    #region NTable
+
+    /// <summary>
+    /// The NTable abstract class.
+    /// </summary>
+    public abstract class NTable : DMTModelBase
+    {
+        #region Static Variables and Properties
+
+        /// <summary>
+        /// sync object used for lock concurrent access.
+        /// </summary>
+        protected static object sync = new object();
+        /// <summary>
+        /// Gets default Connection.
+        /// </summary>
+        public static SQLiteConnection Default { get; set; }
+
+        #endregion
+    }
+
+    #endregion
+}
