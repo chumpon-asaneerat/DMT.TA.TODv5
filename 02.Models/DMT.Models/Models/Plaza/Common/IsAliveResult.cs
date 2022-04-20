129a5ec66be95266f246de1c2b83ac416ac24c0a
chumpon-asaneerat <chumponsenate@gmail.com>
2022-04-20 15:41:18 +0700

    Update 2022-04-20 #13 - DMT.TAxTODv5   - DMT.Models project     - Add IsAliveResult class.     - Move some model folders to plaza sub folder.

---
+++ 02.Models/DMT.Models/Models/Plaza/Common/IsAliveResult.cs
@@ -0,0 +1,38 @@
+﻿#region Using
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection;
+
+using NLib;
+using NLib.Design;
+using NLib.Reflection;
+
+using SQLite;
+using SQLiteNetExtensions.Attributes;
+using SQLiteNetExtensions.Extensions;
+// required for JsonIgnore attribute.
+using Newtonsoft.Json;
+using Newtonsoft.Json.Bson;
+
+#endregion
+
+namespace DMT.Models
+{
+	#region IsAliveResult
+
+	/// <summary>
+	/// The IsAliveResult Model Class.
+	/// </summary>
+	[JsonObject(MemberSerialization.OptOut)]
+	public class IsAliveResult
+	{
+		/// <summary>Gets or sets Time Stamp.</summary>
+		//[PropertyMapName("TimeStamp")]
+		public DateTime? TimeStamp { get; set; }
+	}
+
+	#endregion
+}
