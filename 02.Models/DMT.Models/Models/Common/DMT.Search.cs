f4bfcd29f72b114fde73c3e4b3afb087949c129c
chumpon-asaneerat <chumponsenate@gmail.com>
2022-04-21 22:56:30 +0700

    Update 2022-04-21 #7 - DMT.TAxTODv5   - DMT.Models project     - Update DMT.Searchs classes.

--- 02.Models/DMT.Models/Models/Common/DMT.Search.cs
+++ 02.Models/DMT.Models/Models/Common/DMT.Search.cs
@@ -27,46 +27,6 @@ namespace DMT.Models
 
     #endregion
 
-    #region Revenues
-    /*
-    partial class Search
-    {
-        public static class Revenues
-        {
-            public class PlazaShift : NSearch<PlazaShift>
-            {
-                public UserShift Shift { get; set; }
-                public PlazaGroup PlazaGroup { get; set; }
-
-                public static PlazaShift Create(UserShift shift, PlazaGroup plazaGroup)
-                {
-                    var ret = new PlazaShift();
-                    ret.Shift = shift;
-                    ret.PlazaGroup = plazaGroup;
-                    return ret;
-                }
-            }
-            public class SaveRevenueShift : NSearch<SaveRevenueShift>
-            {
-                public UserShiftRevenue RevenueShift { get; set; }
-                public string RevenueId { get; set; }
-                public DateTime RevenueDate { get; set; }
-
-                public static SaveRevenueShift Create(UserShiftRevenue revenueShift,
-                    string revenueId, DateTime revenueDate)
-                {
-                    var ret = new SaveRevenueShift();
-                    ret.RevenueShift = revenueShift;
-                    ret.RevenueId = revenueId;
-                    ret.RevenueDate = revenueDate;
-                    return ret;
-                }
-            }
-        }
-    }
-    */
-    #endregion
-
     #region Search (Credit)
 
     static partial class Search
@@ -84,7 +44,6 @@ namespace DMT.Models
             {
                 #region Current
 
-                /*
                 /// <summary>
                 /// Gets Current User Credit (By PlazaGroup). 
                 /// State is not complted and has No RevenueId.
@@ -123,13 +82,11 @@ namespace DMT.Models
 
                     #endregion
                 }
-                */
 
                 #endregion
 
                 #region Completed
 
-                /*
                 /// <summary>
                 /// Gets Completed User Credit (By PlazaGroup). 
                 /// State is complted and has No RevenueId.
@@ -168,26 +125,6 @@ namespace DMT.Models
 
                     #endregion
                 }
-                */
-                #endregion
-
-                #region Comment out
-
-                /*
-                public class GetActiveById : NSearch<GetActive>
-                {
-                    public string UserId { get; set; }
-                    public string PlazaGroupId { get; set; }
-
-                    public static GetActiveById Create(string userId, string plazGroupId)
-                    {
-                        var ret = new GetActiveById();
-                        ret.UserId = userId;
-                        ret.PlazaGroupId = plazGroupId;
-                        return ret;
-                    }
-                }
-                */
 
                 #endregion
             }
@@ -209,7 +146,6 @@ namespace DMT.Models
 
             }
 
-            /*
             /// <summary>User Coupon Searchs.</summary>
             public static partial class User
             {
@@ -266,94 +202,9 @@ namespace DMT.Models
 
                 #endregion
             }
-            */
         }
     }
 
 
-    #endregion
-
-    #region UserCoupon (comment out)
-
-    /*
-    partial class Search
-    {
-        public class TSBCoupons
-        {
-            public class ByUser : NSearch<ByUser>
-            {
-                public TSB TSB { get; set; }
-                public User User { get; set; }
-
-                public static ByUser Create(TSB tsb, User user)
-                {
-                    var ret = new ByUser();
-                    ret.User = user;
-                    ret.TSB = tsb;
-                    return ret;
-                }
-            }
-        }
-
-        public class UserCoupons
-        {
-            public class BorrowCoupons : NSearch<BorrowCoupons>
-            {
-                public User User { get; set; }
-                public List<TSBCouponTransaction> Coupons { get; set; }
-
-                public static BorrowCoupons Create(User user, List<TSBCouponTransaction> coupons)
-                {
-                    var ret = new BorrowCoupons();
-                    ret.User = user;
-                    ret.Coupons = coupons;
-                    return ret;
-                }
-            }
-
-            public class ReturnCoupons : NSearch<ReturnCoupons>
-            {
-                public User User { get; set; }
-                public List<TSBCouponTransaction> Coupons { get; set; }
-
-                public static ReturnCoupons Create(User user, List<TSBCouponTransaction> coupons)
-                {
-                    var ret = new ReturnCoupons();
-                    ret.User = user;
-                    ret.Coupons = coupons;
-                    return ret;
-                }
-            }
-
-            public class ByUser : NSearch<ByUser>
-            {
-                public TSB TSB { get; set; }
-                public User User { get; set; }
-
-                public static ByUser Create(TSB tsb, User user)
-                {
-                    var ret = new ByUser();
-                    ret.User = user;
-                    ret.TSB = tsb;
-                    return ret;
-                }
-            }
-
-            public class ToTSBCoupons : NSearch<ToTSBCoupons>
-            {
-                public TSB TSB { get; set; }
-                public List<UserCouponTransaction> Coupons { get; set; }
-
-                public static ToTSBCoupons Create(TSB tsb, List<UserCouponTransaction> coupons)
-                {
-                    var ret = new ToTSBCoupons();
-                    ret.TSB = tsb;
-                    ret.Coupons = coupons;
-                    return ret;
-                }
-            }
-        }
-    }
-    */
     #endregion
 }
